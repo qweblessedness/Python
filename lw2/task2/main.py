0826fca892790b13a9f2dadc7ca7a9139898c394
Андрєєнко Богдан <104623394+qweblessedness@users.noreply.github.com>
2022-09-27 12:48:21 +0300

    Create main.py

---
+++ lw2/task2/main.py
@@ -0,0 +1,9 @@
+import math
+from computer import *
+def function(a,b):
+  Z=(math.sqrt(2)-math.sqrt(3*a))/(2*b)
+  print("Z= ",Z);
+n=int(input("Enter n: "))
+m=int(input("Enter m: "))
+function(n,m)
+computer(n)
